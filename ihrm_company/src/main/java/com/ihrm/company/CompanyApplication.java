a4609b024324113b6fb3e1bb8619280d8a28bdd4
WenYingfei <991440083@qq.com>
2020-06-13 18:02:20 +0800

    eureka的搭建

--- ihrm_company/src/main/java/com/ihrm/company/CompanyApplication.java
+++ ihrm_company/src/main/java/com/ihrm/company/CompanyApplication.java
@@ -4,6 +4,7 @@ import com.ihrm.common.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Bean;
  **/
 @SpringBootApplication(scanBasePackages= "com.ihrm")
 @EntityScan(value = "com.ihrm.domain.company")
+@EnableEurekaClient
 public class CompanyApplication {
 
     public static void main(String[] args) {
--- ihrm_system/src/main/java/com/ihrm/system/SystemApplication.java
+++ ihrm_system/src/main/java/com/ihrm/system/SystemApplication.java
@@ -5,6 +5,9 @@ import com.ihrm.common.utils.JwtUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -14,6 +17,9 @@ import org.springframework.context.annotation.Bean;
  **/
 @SpringBootApplication(scanBasePackages = "com.ihrm")
 @EntityScan(value = "com.ihrm.domain.system")
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
 public class SystemApplication {
 
     public static void main(String[] args) {
